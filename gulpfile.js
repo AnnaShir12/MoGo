e24af146ecee94096825887363775e89af8544d1
AnnaShir12 <9221171109@e1.ru>
2019-12-27 20:55:18 +0500

    remove the plagin un-css, add postCss plagin autoprefixer

--- gulpfile.js
+++ gulpfile.js
@@ -2,10 +2,11 @@
 
 const gulp = require('gulp');
 const concat = require('gulp-concat');
-const uncss = require('gulp-uncss');
 const cleanCSS = require('gulp-clean-css');
 const browserSync = require('browser-sync').create();
 const del = require('del');
+const postcss = require('gulp-postcss');
+var autoprefixer = require('autoprefixer');
 const cssFiles = [
   './src/css/base/base.css',
   './src/css/layout/header-top.css',
@@ -24,19 +25,27 @@ const cssFiles = [
   './src/css/components/services.css',
   './src/css/components/testimonials-slider.css',
   './src/css/components/we-do.css',
-  './src/css/components/test.css',
-  './src/css/layout/footer.css'
+  './src/css/layout/footer.css',
+  './src/css/components/test.css'
 ]
 
 function styles() {
+  var plugins = [
+    autoprefixer({
+      "overrideBrowserslist": [
+        "defaults",
+        "not IE 11",
+        "not IE_Mob 11",
+        "maintained node versions",
+      ]
+    })
+  ];
   return gulp.src(cssFiles)
     .pipe(concat('style.css'))
-    .pipe(uncss({
-      html: ['index.html', 'posts/**/*.html', 'http://example.com']
-    }))
     .pipe(cleanCSS({
       level: 2
     }))
+    .pipe(postcss(plugins))
     .pipe(gulp.dest('./build/css'))
     .pipe(browserSync.stream());
 }
@@ -56,7 +65,6 @@ function watch() {
 }
 
 gulp.task('styles', styles);
-gulp.task('del', clean);
-gulp.task('watch', watch);
-gulp.task('build', gulp.series(clean, gulp.parallel(styles)));
-gulp.task('dev', gulp.series('build', 'watch'));
+gulp.task('clean', clean);
+gulp.task('build', gulp.series(clean, gulp.parallel(styles, watch)));
+gulp.task('default', gulp.series('build'));
